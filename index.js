6efd100c6933a82fdb6eea4454207aa2d58891fb
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-20 14:21:04 -0400

    lab complete

--- index.js
+++ index.js
@@ -21,3 +21,75 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+// My code
+
+function createEmployeeRecord(array){
+    let obj = {}
+    obj.firstName = array[0];
+    obj.familyName = array[1];
+    obj.title  = array[2];
+    obj.payPerHour = array[3];
+    obj.timeInEvents=[]
+    obj.timeOutEvents=[]
+    return obj
+}
+
+
+function createEmployeeRecords(array){
+    return array.map(employee=> createEmployeeRecord(employee))
+}
+
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour:findHour(dateStamp),
+        date: findDate(dateStamp)
+    })
+    return this
+}
+
+function findHour(dateStamp){
+    return Number(dateStamp.slice(11,15))
+}
+
+function findDate(dateStamp){
+    return dateStamp.slice(0,10)
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour:findHour(dateStamp),
+        date: findDate(dateStamp)
+    })
+    return this
+}
+
+function hoursWorkedOnDate(spesDate){
+    let hoursIn = this.timeInEvents.find(record => record.date===spesDate).hour;
+    let hoursOut = this.timeOutEvents.find(record => record.date===spesDate).hour;
+    if (hoursIn&&hoursOut){
+        return ((hoursOut - hoursIn)/100)
+    }
+    return 0;
+}
+
+
+function wagesEarnedOnDate (date){
+    let hoursCounter = hoursWorkedOnDate.bind(this)
+    return (this.payPerHour*hoursCounter(date));
+}
+
+
+function findEmployeeByFirstName (srcArray, firstName){
+    return srcArray.find(employeeRecord => (employeeRecord.firstName===firstName))
+}
+
+
+function calculatePayroll(srcArrayarr){
+    return srcArrayarr.map(employeeRecord => allWagesFor.bind(employeeRecord)()).reduce((a,b)=> a+b,0)
+    
+}
